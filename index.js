8312a3134447ff94df3bcb9c09ce3b2fdd3b9283
Dor <Dorni2@Gmail.com>
2019-01-31 23:21:58 +0200

    WebSocket chat is working

--- index.js
+++ index.js
@@ -3,6 +3,14 @@ const Dal = require('./Dal');
 const app = express()
 const port = 3000
 
+
+let http = require('http');
+let server = http.Server(app);
+let socketPort = 3001;
+
+let socketIO = require('socket.io');
+let io = socketIO(server);
+
 app.get('/', (req, res) => res.send('Hello World!'))
 
 const userRouter = express.Router();
@@ -22,3 +30,20 @@ app.use('/api/users', userRouter);
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
+
+
+
+io.on('connection', (socket) => {
+    console.log('user connected');
+
+    socket.on('new-message', (message) => {
+        console.log(message);
+        io.emit('new-message', message);
+      });
+});
+
+
+
+server.listen(socketPort, () => {
+    console.log(`started on port: ${socketPort}`);
+});
